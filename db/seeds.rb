27b92c6c4dc9bfd342b9d7f277e5ec367d6e0b9e
sean-code <john.nganga@student.moringaschool.com>
2022-11-21 13:27:12 +0300

    Create Seeds and add seeds to the database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Pet.create([{name:"Jack", age:"Harlow"}, {name:"Bryson", age:"Tiller"}])

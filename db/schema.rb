52fb3953df4be7f8e60fc2cb3fce06e7e7ac8e98
sean-code <john.nganga@student.moringaschool.com>
2022-11-21 13:02:40 +0300

    Add column, change column

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_21_093742) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_21_095638) do
   create_table "pets", force: :cascade do |t|
     t.string "name"
     t.string "model"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "age"
   end
 
   create_table "pets2s", force: :cascade do |t|
